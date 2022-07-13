16d90c72424ff6bd0a6cb28e0f1319e1f94886ae
wgs66 <707205590@qq.com>
2022-07-13 12:06:19 +0800

    项目初始化完成

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div>
     <h1>黑马头条</h1>
     <span class="toutiao toutiao-dianzan"></span>
+    <br />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="login"></div>
   </div>
 </template>
 
@@ -13,4 +21,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.login {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import store from './store'
 // 引入iconfoot
 import '@/assets/fonts/iconfont.css'
 
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入flexble
+import 'amfe-flexible/index.min.js'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
